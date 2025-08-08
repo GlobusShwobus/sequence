1ce9a2dcd07254c2241e8db26cfd62d1aaaef2f5
GlobusShwobus <fjodorzaro@gmail.com>
2025-08-08 12:32:25 +0300

    oops

--- sequence/Source.cpp
+++ sequence/Source.cpp
@@ -1,6 +1,11 @@
 #include "Sequence.h"
 #include <vector>
-
+class triangle {
+public:
+	triangle() = default;
+	triangle(int ass) :x(ass) {}
+	int x = 0;
+};
 class Rectangle {
 public:
 	Rectangle() = default;
@@ -19,7 +24,7 @@ public:
 	Rect3& operator=(Rect3&&)noexcept = delete;
 	~Rect3() = default;
 };
-
+#include <iostream>
 using namespace seq;
 int main() {
 
@@ -31,6 +36,17 @@ int main() {
 	//Sequence<Rect3> mmmm;
 	//mmmm.resize(mmmm.capacity() + 1);//should fail, not movable
 
+	Sequence<triangle> wow;
+	Sequence<triangle> wow2 = wow;
+	wow.reserve(200);
+	wow.resize(100);
+
+	for (int i = 0; i < wow.size(); i++) {
+		wow[i] = {i};
+	}
+	wow.shrinkToFit();
+	auto it = wow.begin() + 50;
+	std::cout << it->x << "\n";
 
 	//Sequence<Rectangle> mmmm2 = std::move(mmmm);
 	//std::vector<Rect2> meme;
